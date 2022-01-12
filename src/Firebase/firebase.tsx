eddd8e6dcfa8d475d470c98db3843e0f44ce4bc4
Wilfredo R Colina <wrca26@gmail.com>
2022-01-11 20:17:01 -0600

    adding firebase configuration

---
+++ src/Firebase/firebase.tsx
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCk89LoipIteGprWE5Ne9vTsuI-UrmZD-c",
+    authDomain: "issuestrack-d21ce.firebaseapp.com",
+    projectId: "issuestrack-d21ce",
+    storageBucket: "issuestrack-d21ce.appspot.com",
+    messagingSenderId: "554007848504",
+    appId: "1:554007848504:web:dc0058d034d14b0bda28cd",
+    measurementId: "G-FH08V3WFYF"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebase;
