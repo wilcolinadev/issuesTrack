34ae4b3d2ffc0ad7c03c5388ef377e3f46a06e90
Wilfredo R Colina <wrca26@gmail.com>
2022-01-27 14:01:33 -0600

    bulding the link component for the modal after a successfull signed up

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import {ModalBox, ModalProps, ModalText} from "./ModalStyles";
-
+import {ModalBox, ModalProps, ModalText, ModalLink} from "./ModalStyles";
+import {Link} from "react-router-dom";
 
 
 const Modal:React.FC <ModalProps> = ({active,message})=>(
 
     <ModalBox active={active}>
         <ModalText> {message}</ModalText>
-
+        <Link to={"/login"}>
+            <ModalLink>Log in</ModalLink>
+        </Link>
     </ModalBox>
 );
 
--- src/Components/Modal/ModalStyles.tsx
+++ src/Components/Modal/ModalStyles.tsx
@@ -24,3 +24,23 @@ export const ModalBox = styled.div`
 export const ModalText = styled.h3`
   text-align: center;
 `;
+
+export const ModalLink = styled.h3`
+text-align: center;
+  color: #000;
+  padding-top: 1rem;
+  text-decoration: underline;
+  font-family: 'Quicksand', sans-serif;
+  border: none;
+  font-size: 1rem;
+  background-color: #ccc;
+  font-weight: bolder;
+  filter: drop-shadow(1px 1px 1px #ffffff);
+  margin-top: 1rem;
+  
+  &:hover{
+    background-color: #ffca20 ;
+    transition: 0.4s ease-in-out;
+
+  }
+`;
