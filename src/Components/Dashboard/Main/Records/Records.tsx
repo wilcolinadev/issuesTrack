712cec170f67520a96aa0a0e48348377d5e5a7ba
Wilfredo R Colina <wrca26@gmail.com>
2022-06-21 09:40:10 -0500

    bringing db issues on load

--- src/Components/Dashboard/Main/Records/Records.tsx
+++ src/Components/Dashboard/Main/Records/Records.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RootStateOrAny, useSelector } from "react-redux";
 import Record from "./Record";
+import { getDatabase, set, ref, get } from "firebase/database";
+import { db } from "../../../../Firebase/firebase";
+
 const Records: React.FC = () => {
   const activeIssues = useSelector(
     (state: RootStateOrAny) => state.activeIssues
   );
+
+  const activeUser = useSelector((state: RootStateOrAny) => state.isUserAuth);
+
+  const getRecords = async () => {
+
+      const db = getDatabase();
+      const issuesRef = await ref(db, "issues/" + activeUser.user.uid);
+      const userData = await get(issuesRef);
+    try {
+      console.log(userData.val());
+    } catch (e){
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getRecords().then((response) => {});
+  }, []);
+
   return (
     <>
       {activeIssues.map((issue) => {
